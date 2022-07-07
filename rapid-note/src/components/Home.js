9b94da507b024f622d81f53d4a2fb054757df30a
cfape <c.penagos16@gmail.com>
2022-07-07 13:53:51 -0500

    remote

--- rapid-note/src/components/Home.js
+++ rapid-note/src/components/Home.js
@@ -4,7 +4,6 @@ import { useAuth } from '../contex/authContext.js';
 import logo from '../img/logo.png';
 import logotext from '../img/logotext.png';
 
-
 export function Home() {
 
   const { user } = useAuth()
