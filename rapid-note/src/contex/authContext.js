73645b927a0395e048e9c73f6acd37493c3fd521
cfape <c.penagos16@gmail.com>
2022-07-09 23:15:58 -0500

    authContext

--- rapid-note/src/contex/authContext.js
+++ rapid-note/src/contex/authContext.js
@@ -17,11 +17,16 @@ export const useAuth = () => {
   return context
 }
 
+export const signUp = ( email, password) =>
+    createUserWithEmailAndPassword( auth, email, password);
+    console.log('hola')
+
 export const login = (email, password) => signInWithEmailAndPassword(auth, email, password);
+console.log('borrar');
 
 
 export   const loginWithGoogle = () => {
-  console.log ('bbbbbbb');
+  console.log('borrarte');
   const googleProvider = new GoogleAuthProvider()
   return signInWithPopup(auth, googleProvider)
   }
@@ -31,18 +36,9 @@ export function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
 
-  const signUp = ( email, password) =>
-    createUserWithEmailAndPassword( auth, email, password);
-
-  const loginNote = (email, password) =>
-    signInWithEmailAndPassword(auth, email, password);
 
   const logOut = () => signOut(auth);
-
-  const loginWithGoogle = () => {
-    const googleProvider = new GoogleAuthProvider()
-    return signInWithPopup(auth, googleProvider)
-    }
+  console.log('sirve');
 
     useEffect (() => {
       onAuthStateChanged(auth, currentUser => {
@@ -52,6 +48,6 @@ export function AuthProvider({ children }) {
     },[])
 
   return (
-  <authContext.Provider value={{ signUp, loginNote, user, logOut, loading, loginWithGoogle }}>{children}</authContext.Provider>
+  <authContext.Provider value={{ user, logOut, loading }}>{children}</authContext.Provider>
   );
 }
